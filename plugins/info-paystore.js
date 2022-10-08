3b89b6a1a10ee28a47ed824616feeda38b87e296
BiyssBot <115330137+BiyssBot@users.noreply.github.com>
2022-10-08 09:09:20 +0700

    Update info-paystore.js

--- plugins/info-paystore.js
+++ plugins/info-paystore.js
@@ -1,33 +1,18 @@
 let handler = async (m, { conn }) => {
 	//-----PRICE
 //sewa
-let sh = '5'
-let sn = '15'
-let ss = '30'
-let sp = '35'
-let sv = '65'
-//premium
-let ph = '5'
-let pn = '20'
-let pp = '40'
-let pv = '50'
-let ppm = '70'
-let info = `
+let sh = '4'
+let sn = '8'
+let ss = '12'
+let sp = '15'
+
 ╭━━━━「 *SEWA* 」
 ┊⫹⫺ *Hemat:* _${sh}k/grup (1 minggu)_
 ┊⫹⫺ *Normal:* _${sn}k/grup (1 bulan)_
-┊⫹⫺ *Standar:* _${ss}k/grup (2 bulan)_
-┊⫹⫺ *Pro:* _${sp}k/grup (4 bulan)_                                                      
-┊⫹⫺ *Vip:* = _${sv}k/grup (12 bulan)_
+┊⫹⫺ *Standar:* _${ss}k/grup (3 bulan)_
+┊⫹⫺ *Pro:* _${sp}k/grup (permanent)_                                                      
 ╰═┅═━––––––๑
-
-╭━━━━「 *PREMIUM* 」
-┊⫹⫺ *Hemat:* _${ph}k (1 minggu)_
-┊⫹⫺ *Normal:* _${pn}k (1 bulan)_
-┊⫹⫺ *Pro:* _${pp}k (4 bulan)_
-┊⫹⫺ *Vip:* _${pv}k (8 bulan)_                                               
-┊⫹⫺ *Permanent (jual script bot):* = _${ppm}k (Unlimited)_
-╰═┅═━––––––๑
+       
 
 *⫹⫺ PAYMENT:*
 • *Pulsa Telkomsel:* [${ppulsa}]
